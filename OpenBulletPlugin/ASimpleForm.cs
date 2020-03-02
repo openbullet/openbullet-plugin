5424aa79575c19297cfac555a0f43b7d1e9f6ae4
ruri <ruridev@protonmail.com>
2020-03-02 03:43:19 +0100

    ASimpleForm and BlockSum

---
+++ OpenBulletPlugin/ASimpleForm.cs
@@ -0,0 +1,158 @@
+﻿using PluginFramework;
+using PluginFramework.Attributes;
+using RuriLib;
+using RuriLib.Interfaces;
+using RuriLib.Models;
+using RuriLib.ViewModels;
+using System.IO;
+
+namespace OpenBulletPlugin
+{
+    /*
+     * ============================================================================================================
+     * 
+     * -- INTRODUCTION --
+     * This is a sample plugin that will appear inside the Plugins section of OpenBullet.
+     * Make sure to read all comments to understand what everything is doing, and feel free to experiment with it.
+     * 
+     * -- BUILDING --
+     * This project must reference RuriLib and PluginFramework, which are available at https://github.com/openbullet/openbullet
+     * and are also included in this sample project (but they will not be kept up to date).
+     * When you compile this project, it will output a DLL file that you need to place in OpenBullet's Plugins folder,
+     * and then restart OpenBullet in order for it to work.
+     * 
+     * -- DEPENDENCIES --
+     * If you want to load any dependencies you will be able to do so by moving all the DLLs into a subfolder with the
+     * same name as the master DLL, for example something like this:
+     * Plugins/
+     * Plugins/Example.dll
+     * Plugins/Example/Dependency.dll
+     * 
+     * Note: If you want to add a dependency that OpenBullet already depends on (e.g. Newtonsoft.Json) you MUST make sure
+     * to use the EXACT SAME VERSION that OpenBullet uses when you compile the plugin (it's easy to just copy the DLL file from
+     * OpenBullet's folder and reference that instead of the nuget package which might be more updated), and finally you MUST NOT INCLUDE
+     * the DLL of that dependency in the subfolder since OpenBullet already loads it by default.
+     * 
+     * ============================================================================================================
+     */
+
+    /*
+     * Each class will make a separate entry in the plugins dropdown menu, so a single DLL can contain multiple plugins inside.
+     * For a normal plugin, you must implement the IPlugin interface.
+     */
+    public class ASimpleForm : IPlugin
+    {
+        // The name of the plugin shown in the dropdown box
+        public string Name => "A Simple Form";
+
+        /*
+         * This property will be linked to a single-line textbox in the user interface.
+         * The property MUST be of type string.
+         * The Text attribute is here applied to the property FirstName to specify that it needs to be drawn as a textbox.
+         * The attribute accepts 2 parameters:
+         * - a label (which will be shown right next to the textbox)
+         * - a tooltip (which will be shown on mouse hover) (optional)
+         * You can also set a default value for the field, for example in this case it's "John".
+         * 
+         * ONLY properties with a valid attribute will be drawn on the interface, other properties can still be used in the class for
+         * coding purposes but they will be disregarded by the interface builder.
+         */
+        [Text("First Name", "Your first name... duh")]
+        public string FirstName { get; set; } = "John";
+
+        [Text("Last Name", "Your last name... duh")]
+        public string LastName { get; set; } = "Doe";
+
+        /*
+         * This property will be linked to a numerical up-down control in the user interface.
+         * The property MUST be of type int.
+         * The Numeric attribute accepts 4 parameters:
+         * - a label
+         * - a tooltip (optional)
+         * - the minimum acceptable value
+         * - the maximum acceptable value
+         */
+        [Numeric("Age", minimum = 0, maximum = 100)]
+        public int Age { get; set; } = 18;
+
+        /*
+         * This property will be linked to a checkbox in the user interface.
+         * The property MUST be of type bool.
+         * The Checkbox attribute only accepts a label and optional tooltip.
+         */
+        [Checkbox("Are you tall?")]
+        public bool Tall { get; set; } = true;
+
+        /*
+         * This property will be linked to a multi-line textbox in the user interface.
+         * The property MUST be of type string[].
+         * When the user inputs values in the textbox, they will be returned in an array where each element is a line of the textbox.
+         * The TextMulti attribute only accepts a label and an optional tooltip.
+         */
+        [TextMulti("Interests", "Type one interest per line")]
+        public string[] Interests { get; set; } = new string[] { "Soccer", "Jogging", "Fishing" };
+
+        /*
+         * This property will be linked to an Open File Dialogue (allows to select a file from disk) in the user interface.
+         * The property MUST be of type string and will contain the full path to the file chosen by the user.
+         * The FilePicker attribute only accepts a label and an optional tooltip.
+         */
+        [FilePicker("File to open", "The file you want to read")]
+        public string FileToRead { get; set; }
+
+        /*
+         * This property will be linked to a Dropdown Box in the user interface.
+         * The property MUST be of type string and it will contain the option chosen by the user.
+         * The Dropdown attribute accepts a label and optional tooltip, and a list of options for the dropdown.
+         * You can specify the option to select at the beginning via the default property value.
+         */
+        [Dropdown("Order", "What would you like to drink?", options = new string[] { "Coke", "Pepsi", "Sprite", "Fanta" })]
+        public string Order { get; set; } = "Coke";
+
+        /*
+         * This property will be linked to a Wordlist Picker Dialogue in the user interface.
+         * The property MUST be of type Wordlist and it will contain the Wordlist model chosen by the user.
+         * The WordlistPicker attribute only accepts a label and an optional tooltip.
+         */
+        [WordlistPicker("Wordlist", "The wordlist you want to choose")]
+        public Wordlist Wordlist { get; set; }
+
+        /*
+         * This property will be linked to a Config Picker Dialogue in the user interface.
+         * The property MUST be of type ConfigViewModel and it will contain the viewmodel of the config chosen by the user.
+         * The ConfigPicker attribute only accepts a label and an optional tooltip.
+         */
+        [ConfigPicker("Config", "The config you want to choose")]
+        public ConfigViewModel Config { get; set; }
+
+        /*
+         * This method will be linked to a Button in the user interface and will be executed whenever the user pressed the button.
+         * The method MUST be of type void and accept one parameter of type IApplication.
+         * 
+         * IApplication is a group of interfaces that allow the plugin to talk to most of OpenBullet's components, such as Runners,
+         * the Logger or the Config Manager. This is where the power of the plugin system actually resides, and it allows you to provide
+         * additional functionalities to OpenBullet without the need to modify the original source code.
+         * 
+         * ONLY methods with a valid attribute will be linked to buttons on the interface, other methods can still be used in the class for
+         * coding purposes but they will be disregarded by the interface builder.
+         */
+        [Button("Show Message Box")]
+        public void Execute(IApplication app)
+        {
+            var isTall = Tall ? "" : " not";
+            var interests = string.Join(", ", Interests);
+            var fileContent = string.IsNullOrEmpty(FileToRead) ? "INVALID FILE" : File.ReadAllText(FileToRead);
+            var wordlistLines = Wordlist == null ? 0 : Wordlist.Total;
+            var configBlocks = Config == null ? 0 : Config.Config.BlocksAmount;
+
+            /*
+             * The Log method of the Logger will log a message to the system log with the specified level, and in this case
+             * it will also be prompted to the user through a MessageBox.
+             */
+            app.Logger.Log($@"Hello {FirstName} {LastName} you're {Age} years old and you're{isTall} tall. You like {interests}.
+The content of the file is {fileContent}.
+You would like to order some {Order}.
+Your wordlist has {wordlistLines} lines and your config has {configBlocks} blocks.", LogLevel.Info, true);
+        }
+    }
+}
---
+++ OpenBulletPlugin/BlockSum.cs
@@ -0,0 +1,198 @@
+﻿using PluginFramework;
+using PluginFramework.Attributes;
+using RuriLib;
+using RuriLib.LS;
+using System;
+using System.Collections.Generic;
+
+namespace OpenBulletPlugin
+{
+    /*
+     * ============================================================================================================
+     * 
+     * -- INTRODUCTION --
+     * This is a sample block plugin that will appear inside the Add Block section of OpenBullet's Stacker.
+     * Make sure to read all comments to understand what everything is doing, and feel free to experiment with it.
+     * 
+     * For more information about building and dependencies, please refer to the comments in the ASimpleForm.cs class.
+     * 
+     * ============================================================================================================
+     */
+
+    /*
+     * Each class will make a separate block plugin, so a single DLL can contain multiple block plugins inside.
+     * For a block plugin, you must inherit from BlockBase and implement the IBlockPlugin interface.
+     */
+    public class BlockSum : BlockBase, IBlockPlugin
+    {
+        // The default label of the block when it's added
+        public string Name => "SUM";
+
+        /* The color of the block. This must contain a string with one of the following color names
+         * http://www.flounder.com/csharp_color_table.htm
+         */
+        public string Color => "Cyan";
+
+        /*
+         * If this is true, the label of the block will be displayed with a light font instead of a dark one.
+         * Use it when your background color is very dark, in order to increase contrast.
+         */
+        public bool LightForeground => false;
+
+        /*
+         * Please refer to the comments in the ASimpleForm.cs class for an overview of attributes and properties.
+         */
+        [Text("First Number", "The first operand")]
+        public string First { get; set; } = "1";
+
+        [Text("Second Number", "The second operand")]
+        public string Second { get; set; } = "2";
+
+        [Text("Variable Name", "The output variable name")]
+        public string VariableName { get; set; } = "";
+
+        [Checkbox("Is Capture", "Should the output variable be marked as capture?")]
+        public bool IsCapture { get; set; } = false;
+
+        /*
+         * This is the constructor of the block, it MUST accept ZERO arguments.
+         */
+        public BlockSum()
+        {
+            Label = Name;
+        }
+
+        /*
+         * This method will read and build the block from a LoliScript statement.
+         * You can take a look at similar methods from other existing blocks in the original OpenBullet source to see how to
+         * do it, but I will try to explain everything that is done here so that even new coders can understand easily.
+         * 
+         * The FromLS method will override the default FromLS method of the abstract BlockBase class and will accept as argument
+         * the single LoliScript directive that specifies the block and its options (as you know in LoliScript each line is a separate statement).
+         */
+        public override BlockBase FromLS(string line)
+        {
+            // We trim the input in case the user added any spaces at the start or at the end.
+            var input = line.Trim();
+
+            /* We parse the label of the block, for example if someone types
+             * #MYLABEL SUM ...
+             * it means that the SUM block has a label named 'MYLABEL'.
+             * So we need to parse this value in order to correctly show it in the stacker.
+             */
+            if (input.StartsWith("#")) // If the input actually has a label
+                Label = LineParser.ParseLabel(ref input); // Parse the label and remove it from the original string
+
+            /*
+             * The syntax of this very simple block is the following (square brackets mean 'optional'):
+             * SUM "FIRST" "SECOND" [-> VAR/CAP "NAME"]
+             * */
+
+            /*
+             * A literal is an argument between double quotes, which usually represents a string.
+             * When we parse a literal, the LineParser will make sure that the parameter is double-quoted.
+             * 
+             * The first parameter is the reference to the input string, while the second is needed to display
+             * to the user that a certain parameter is missing from the statement in case it's required.
+             */
+            First = LineParser.ParseLiteral(ref input, "First Number");
+            Second = LineParser.ParseLiteral(ref input, "Second Number");
+
+            /* Try to parse the arrow. Remember that the arrow is optional, a user does not necessarily need to
+             * provide an output variable (even though it makes little sense in this example), so we try to parse
+             * an arrow token. If the returned value is empty, then it means that it didn't find an arrow and 
+             * we need to return the block as-is, without modifying any other property because the input is empty.
+             */
+            if (LineParser.ParseToken(ref input, TokenType.Arrow, false) == "")
+                return this;
+
+            /*
+             * If we are here, it means that the user specified a certain VAR or CAP to output to, so
+             * we need to parse those and set their values inside the block.
+             */
+            try
+            {
+                // We parse VAR or CAP, and depending on its value we set IsCapture to true or false. If not specified, throw an exception.
+                var varType = LineParser.ParseToken(ref input, TokenType.Parameter, true);
+                if (varType.ToUpper() == "VAR" || varType.ToUpper() == "CAP")
+                    IsCapture = varType.ToUpper() == "CAP";
+            }
+            catch { throw new ArgumentException("Invalid or missing variable type"); }
+
+            // We try to parse the VAR or CAP name. If it was not specified, throw an exception.
+            try { VariableName = LineParser.ParseToken(ref input, TokenType.Literal, true); }
+            catch { throw new ArgumentException("Variable name not specified"); }
+
+            // Now we can finally return the block as there is nothing else to parse.
+            return this;
+        }
+
+        /*
+         * This method basically does the opposite of the method above, so it converts a Block from a class to an actual
+         * LoliScript statement. It's much easier to write since the BlockWriter was implemented with a fluent pattern.
+         */
+        public override string ToLS(bool indent = true)
+        {
+            /*
+             * We initialize a new BlockWriter with the given Block type (in this case, GetType() will return BlockSum)
+             * and indentation enabled by default when needed (you can type .Indent() anywhere to indent to the next line).
+             * The Disabled parameter will tell the writer if the block has been disabled. In that case, it will put a '!'
+             * in front of the statement to indicate that it shouldn't be executed by a debugger or runner.
+             */
+            var writer = new BlockWriter(GetType(), indent, Disabled)
+                .Label(Label) // Write the label. If the label is the default one, nothing is written.
+                .Token(Name) // Write the block name. This cannot be changed.
+                .Literal(First) // Write the 'First' parameter as a literal (it will be double quoted).
+                .Literal(Second); // Write the 'Second' parameter as a literal.
+
+            // Check if the VariableName is the default one. If it is, we don't need to write it to the string.
+            if (!writer.CheckDefault(VariableName, nameof(VariableName)))
+            {
+                // Here we reutilize the writer to write more stuff.
+                writer
+                     .Arrow() // Write the -> arrow.
+                     .Token(IsCapture ? "CAP" : "VAR") // Write CAP or VAR depending on IsCapture.
+                     .Literal(VariableName); // Write the Variable Name as a literal.
+            }
+
+            // Finally we call the ToString() method of the writer to return the statement that will be written to the LoliScript code.
+            return writer.ToString();
+        }
+
+        /*
+         * This method will run whenever a block is processed by a Runner or Debugger.
+         * It overrides the same method of the BlockBase abstract class.
+         */
+        public override void Process(BotData data)
+        {
+            // Call the Process method of the BlockBase abstract class for any pre-processing needed.
+            base.Process(data);
+
+            /* Replace variables in First and Second, for example if a user wrote
+             * SUM "<VAR1>" "<VAR2>"
+             * we want to replace <VAR1> and <VAR2> with their actual stored values, and then change their type from string to int.
+             */
+            var first = int.Parse(ReplaceValues(First, data));
+            var second = int.Parse(ReplaceValues(Second, data));
+            
+            // We compute the result as a string (remember that OpenBullet only accepts strings, or list of strings, or dictionaries of strings).
+            var result = (first + second).ToString();
+
+            // We create a list of outputs with a single element (this is an old system that needs a rework, sorry).
+            var list = new List<string>() { result };
+
+            /* This method will add the variables from the list into OpenBullet. Hover on the name of the method
+             * to see what all the parameters mean.
+             */
+            InsertVariables(data, IsCapture, false, list, VariableName, "", "", false, false);
+
+            /*
+             * We use the Log method of data to output information to the debugger log / bot log. If you want to use a
+             * different color, you can reference the System.Drawing assembly and pass a LogEntry with the desired color.
+             * 
+             * Note: do not use data.LogBuffer.Add() since it will not check if the user has disabled bot logging.
+             */
+            data.Log($"Added {first} and {second} with result {result}");
+        }
+    }
+}
